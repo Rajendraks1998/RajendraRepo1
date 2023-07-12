704919bdc3118b20923b664d32b5cb5824b90440
Rajendra <rajendraks527@gmail.com>
2023-07-12 09:40:31 +0530

    First commit

---
+++ DemoProject/src/com/git/mypack/DenoClass.java
@@ -0,0 +1,15 @@
+package com.git.mypack;
+
+public class DenoClass {
+
+	public static void main(String[] args) {
+		int wedit=89;
+		int heigth=34;
+		int depth=45;
+		
+		System.out.println(depth);
+		
+	}
+	
+
+}
